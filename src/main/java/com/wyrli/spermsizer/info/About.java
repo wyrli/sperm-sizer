15115e7eb492bf3c8b1b9939541f644ded507480
wyrli <43380232+wyrli@users.noreply.github.com>
2025-10-26 12:33:46 +1100

    Update version to 1.7

--- src/main/java/com/wyrli/spermsizer/info/About.java
+++ src/main/java/com/wyrli/spermsizer/info/About.java
@@ -3,8 +3,8 @@ package com.wyrli.spermsizer.info;
 import com.wyrli.spermsizer.Main;
 
 public class About {
-	public static final String VERSION_INFO = "Version 1.6.6 (16 May 2021)";
-	public static final String COPYRIGHT = "Copyright " + '\u00A9' + " 2020-2021 Roger Li. All rights reserved.";
+	public static final String VERSION_INFO = "Version 1.7 (26 October 2025)";
+	public static final String COPYRIGHT = "Copyright " + '\u00A9' + " 2020-2025 Roger Li. All rights reserved.";
 	public static final String GITHUB_LINK = "https://github.com/wyrli/sperm-sizer";
 
 	public static void show() {
